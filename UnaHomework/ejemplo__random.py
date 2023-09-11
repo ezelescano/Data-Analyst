c4fc366532831bf021a478ffb2a76668a0fec117
Ezequiel <ezequiel.lesca@gmail.com>
2023-09-11 17:45:12 -0300

    Terminado

---
+++ UnaHomework/ejemplo__random.py
@@ -0,0 +1,6 @@
+#importamos el módulo random (esto tiene que estar arriba de todo)
+import random
+#genero un número entero al azar, entre paréntesis se explicita el rango, que incluye dichos valores.
+numero = random.randint(0, 100)
+#imprimo para probar
+print(numero)
